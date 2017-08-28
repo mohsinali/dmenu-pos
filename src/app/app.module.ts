81641af0ddf4ebc89184d6b57c4cc61f2e15fa87
Mohsin Ali <mohsin@Mohsins-MacBook-Pro.local>
2017-08-28 21:30:02 +0500

    Materialize theme implementation - Final

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from '@angular/material';
+import 'hammerjs';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
@@ -8,7 +11,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
