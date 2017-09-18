23413b741b372e63fc91c1918c3f9b7fbf88a1b8
Mohsin Ali <mohsin@Mohsins-MacBook-Pro.local>
2017-09-18 11:33:51 +0500

    Add to cart and increase quantity

--- src/app/menu-items/menu-items.component.ts
+++ src/app/menu-items/menu-items.component.ts
@@ -4,6 +4,7 @@ import { CategoryService } from './../services/categories.service';
 import { Category } from './../models/category';
 import { MenuItem } from './../models/menu-item';
 import { CartItem } from './../models/cart-item';
+import * as _ from "lodash";
 
 @Component({
   selector: 'menu-items',
@@ -25,20 +26,25 @@ export class MenuItemsComponent implements OnInit {
   
     let cItem: CartItem;
     if(this.cartItems.length > 0){
+      let index = _.findIndex(this.cartItems, (i) => {
+        return item.id === i.id;
+      });
 
+      if(index === -1){ // Not found
+        cItem = {id: item.id, name: item.name, quantity: 1, price: item.price};
+        this.cartItems.push(cItem);
+
+      }else{ // Item found, increase quantity
+        cItem = this.cartItems[index];
+        cItem.quantity = cItem.quantity + 1;
+        this.cartItems[index] = cItem;
+      }
+      
+      console.log(this.cartItems);
     }else{
-      cItem = {id: item.id, name: item.name, quantity: 1, price: 5};
+      cItem = {id: item.id, name: item.name, quantity: 1, price: item.price};
       this.cartItems.push(cItem);
     }
-    
-    // c_i.id = 5;
-    // c_i.name = 'Mohsin';
-    
-
-    // if(this.cartItems.length > 0)
-    //   this.cartItems.push(c_i);
-
-    // console.log(c_i);
   }
 
 }
